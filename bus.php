be39462d0c61887b8ec71cf288a380bd26a57571
9pab <piboonsak@gmail.com>
2021-07-08 13:18:48 +0700

    update

--- bus.php
+++ bus.php
@@ -1,4 +1,6 @@
 <?php
+$cookie_name = "empid";
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
 
 $empid = "empid";
 $route = $_GET["r"];
@@ -6,11 +8,13 @@ $seat = $_GET["s"];
 date_default_timezone_set("Asia/Bangkok");
 $date = date("Y/m/d - H:i:s");
 
-if(!isset($_COOKIE[$empid])) {
-    echo "Cookie named '" . $empid . "' is not set!<br>";
+if(!isset($_COOKIE[$cookie_name])) {
+    echo "Cookie named '" . $cookie_name . "' is not set!<br>";
+    echo "<form action='bus.php?r=C11&s=45' method='post'> Name: <input type='number' name='e'><br><input type='submit'>";
+
   } else {
-    echo "Cookie '" . $empid . "' is set!<br>";
-    echo "Value is: " . $_COOKIE[$empid];
+    echo "Cookie '" . $cookie_name . "' is set!<br>";
+    echo "Value is: " . $_COOKIE[$cookie_name];
   }
   
     echo "สายรถ : " . $route . " หมายเลขที่นั่ง : " . $seat . " วันที่/เวลา : " . $date;
