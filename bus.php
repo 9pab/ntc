90d9b498d1083998be914218e578306fed62139b
9pab <piboonsak@gmail.com>
2021-07-08 12:50:43 +0700

    update

--- bus.php
+++ bus.php
@@ -1,5 +1,5 @@
 <?php
     date_default_timezone_set("Asia/Bangkok");
-    echo $_GET["route"] . $_GET["seat"] . date("Y/m/d - H:i:s");
+    echo "สายรถ : " . $_GET["route"] . " หมายเลขที่นั่ง : " . $_GET["seat"] . " วันที่/เวลา : " . date("Y/m/d - H:i:s");
 
 ?>
