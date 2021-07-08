062a044ac0dde0cdbb8684cdb8a74fa9556d4479
9pab <piboonsak@gmail.com>
2021-07-08 12:56:30 +0700

    update

--- bus.php
+++ bus.php
@@ -1,4 +1,12 @@
 <?php
+
+if(!isset($_COOKIE[$empid])) {
+    echo "Cookie named '" . $empid . "' is not set!";
+  } else {
+    echo "Cookie '" . $empid . "' is set!<br>";
+    echo "Value is: " . $_COOKIE[$empid];
+  }
+  
     date_default_timezone_set("Asia/Bangkok");
     echo "สายรถ : " . $_GET["route"] . " หมายเลขที่นั่ง : " . $_GET["seat"] . " วันที่/เวลา : " . date("Y/m/d - H:i:s");
 
